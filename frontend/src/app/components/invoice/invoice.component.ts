51825db60378d26193a66a9900e12151f5fe2ddf
NR <contact@nicolasraimbaud.fr>
2025-01-30 19:35:10 +0100

    Amélioration UX Invoice

--- frontend/src/app/components/invoice/invoice.component.ts
+++ frontend/src/app/components/invoice/invoice.component.ts
@@ -3,6 +3,7 @@ import { InvoiceListComponent } from '../invoice-list/invoice-list.component';
 import { InvoiceFormComponent } from '../invoice-form/invoice-form.component';
 import { InvoicePreviewComponent } from '../invoice-preview/invoice-preview.component';
 import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-invoice',
@@ -14,7 +15,13 @@ import { RouterModule } from '@angular/router';
 export class InvoiceComponent {
   ocrData = { rows: [] };
 
+  constructor(private router: Router) {}
+
   handleOcrResults(results: any): void {
     this.ocrData = results;
+  }
+
+  validateChanges(): void {
+    this.router.navigate(['/', 'invoices']);
   }
 }
